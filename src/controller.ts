e7629fb1a058c8af0326060dee5a90f5e9eaee1c
saintpreston <90055250+Saintpreston@users.noreply.github.com>
2023-04-11 13:43:04 -0400

    configure es-lint

--- src/controller.ts
+++ src/controller.ts
@@ -11,46 +11,46 @@ import { Class,create } from "./utils";
  
 
 export class TodoController {
- model: TodoModel;
- view!: TodoView;
- private viewContructor!: Class<TodoView>;
+	model: TodoModel;
+	view!: TodoView;
+	private viewContructor!: Class<TodoView>;
 
- constructor(model: Class<TodoModel>, viewConstructor: Class<TodoView>) {
-   this.model = create(model);
-   this.model.bindTodoListChanged(this.onTodoListChanged);
-   this.viewContructor = viewConstructor;
- }
+	constructor(model: Class<TodoModel>, viewConstructor: Class<TodoView>) {
+		this.model = create(model);
+		this.model.bindTodoListChanged(this.onTodoListChanged);
+		this.viewContructor = viewConstructor;
+	}
 
- onTodoListChanged = (todos: Todo[]) => {
-   this.view.displayTodos(todos)
- }
+	onTodoListChanged = (todos: Todo[]) => {
+		this.view.displayTodos(todos);
+	};
 
- handleAddTodo = (todoText: string) => {
-   this.model.addTodo(todoText)
- }
+	handleAddTodo = (todoText: string) => {
+		this.model.addTodo(todoText);
+	};
 
- handleEditTodo = (id: number, todoText: string) => {
-   this.model.editTodo(id, todoText)
- }
+	handleEditTodo = (id: number, todoText: string) => {
+		this.model.editTodo(id, todoText);
+	};
 
- handleDeleteTodo = (id: number) => {
-   this.model.deleteTodo(id)
- }
+	handleDeleteTodo = (id: number) => {
+		this.model.deleteTodo(id);
+	};
 
- handleToggleTodo = (id: number) => {
-   this.model.toggleTodo(id)
- }
+	handleToggleTodo = (id: number) => {
+		this.model.toggleTodo(id);
+	};
 
- mount(root : HTMLElement){
+	mount(root : HTMLElement){
 
-  this.view = create(this.viewContructor,root);
+		this.view = create(this.viewContructor,root);
   
   
-  this.view.bindAddTodo(this.handleAddTodo)
-  this.view.bindEditTodo(this.handleEditTodo)
-  this.view.bindDeleteTodo(this.handleDeleteTodo)
-  this.view.bindToggleTodo(this.handleToggleTodo)
-  this.view.displayTodos(this.model.todos)
- }
+		this.view.bindAddTodo(this.handleAddTodo);
+		this.view.bindEditTodo(this.handleEditTodo);
+		this.view.bindDeleteTodo(this.handleDeleteTodo);
+		this.view.bindToggleTodo(this.handleToggleTodo);
+		this.view.displayTodos(this.model.todos);
+	}
 
 }
--- src/model.ts
+++ src/model.ts
@@ -3,17 +3,17 @@
 
 
 export class Todo {
- id: number;
- text: string;
- isComplete: boolean;
- dueDate?: Date;
+	id: number;
+	text: string;
+	isComplete: boolean;
+	dueDate?: Date;
 
- constructor(id: Todo["id"], text: Todo["text"], isComplete: Todo["isComplete"], dueDate: Todo["dueDate"]){
-  this.id = id;
-  this.text= text;
-  this.isComplete = isComplete;
-  this.dueDate = dueDate;
- }
+	constructor(id: Todo["id"], text: Todo["text"], isComplete: Todo["isComplete"], dueDate: Todo["dueDate"]){
+		this.id = id;
+		this.text= text;
+		this.isComplete = isComplete;
+		this.dueDate = dueDate;
+	}
 }
 
 /**
@@ -25,71 +25,71 @@ export class Todo {
 
 
 export class TodoModel {
- todos: Todo[];
- onTodoListChanged?: (todos: Todo[]) => void
+	todos: Todo[];
+	onTodoListChanged?: (todos: Todo[]) => void;
  
- constructor() {
+	constructor() {
 
-   const localStorageTodos = localStorage.getItem('todos');
+		const localStorageTodos = localStorage.getItem("todos");
 
-   if(localStorageTodos !== null){
-    this.todos = JSON.parse(localStorageTodos);
-    return
-   } else{
-    this.todos =  []
-   }
+		if(localStorageTodos !== null){
+			this.todos = JSON.parse(localStorageTodos);
+			return;
+		} else{
+			this.todos =  [];
+		}
  
  
    
- }
+	}
 
-//onTodoListChanged(todos: Todo[]): void
+	//onTodoListChanged(todos: Todo[]): void
 
- bindTodoListChanged(callback : (todos: Todo[]) => void) {
-    this.onTodoListChanged = callback
- }
+	bindTodoListChanged(callback : (todos: Todo[]) => void) {
+		this.onTodoListChanged = callback;
+	}
 
- _commit(todos: Todo[]) {
-  if(this.onTodoListChanged){
-    this.onTodoListChanged(todos)
-   localStorage.setItem('todos', JSON.stringify(todos));
-   return
-  }
-   throw new Error("Bind onTodoListChanged to the View method that rerenders the dom")
- }
+	_commit(todos: Todo[]) {
+		if(this.onTodoListChanged){
+			this.onTodoListChanged(todos);
+			localStorage.setItem("todos", JSON.stringify(todos));
+			return;
+		}
+		throw new Error("Bind onTodoListChanged to the View method that rerenders the dom");
+	}
 
- addTodo(todoText: string) {
-   const todo: Todo= {
-     id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
-     text: todoText,
-     isComplete: false,
-   }
+	addTodo(todoText: string) {
+		const todo: Todo= {
+			id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+			text: todoText,
+			isComplete: false,
+		};
 
-   this.todos.push(todo)
+		this.todos.push(todo);
 
-   this._commit(this.todos)
- }
+		this._commit(this.todos);
+	}
 
- editTodo(id: Todo["id"], updatedText: Todo["text"]) {
+	editTodo(id: Todo["id"], updatedText: Todo["text"]) {
 
-   this.todos = this.todos.map(todo =>
-     todo.id === id ? { id: todo.id, text: updatedText, isComplete: todo.isComplete } : todo
-   )
+		this.todos = this.todos.map(todo =>
+			todo.id === id ? { id: todo.id, text: updatedText, isComplete: todo.isComplete } : todo
+		);
 
-   this._commit(this.todos)
- }
+		this._commit(this.todos);
+	}
 
- deleteTodo(id: Todo["id"]) {
-   this.todos = this.todos.filter(todo => todo.id !== id)
+	deleteTodo(id: Todo["id"]) {
+		this.todos = this.todos.filter(todo => todo.id !== id);
 
-   this._commit(this.todos)
- }
+		this._commit(this.todos);
+	}
 
- toggleTodo(id: Todo["id"]) {
-   this.todos = this.todos.map(todo =>
-     todo.id === id ? { id: todo.id, text: todo.text, isComplete: !todo.isComplete } : todo
-   )
+	toggleTodo(id: Todo["id"]) {
+		this.todos = this.todos.map(todo =>
+			todo.id === id ? { id: todo.id, text: todo.text, isComplete: !todo.isComplete } : todo
+		);
 
-   this._commit(this.todos)
- }
+		this._commit(this.todos);
+	}
 }
